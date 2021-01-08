6186873767ee8a2728ce1ed7c807d21a3f03fb32
Kelvin Quinonez <kesqui7@gmail.com>
2021-01-07 20:13:12 -0600

    Setting up a new page for creating a new party room.

--- music_fezt/api/views.py
+++ music_fezt/api/views.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from rest_framework import generics
+from rest_framework import generics, status
 from django.shortcuts import render
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class RoomView(generics.CreateAPIView):
--- music_fezt/frontend/views.py
+++ music_fezt/frontend/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+
 
 # Create your views here.
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+
